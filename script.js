c6a9edd3a9ff7e541c3cdec877aee1392917494b
Anhlh <anhlh@aladintech.co>
2023-09-14 09:16:24 +0700

    I modified the script.js to let user input how many rounds of R-P-S they wanna play and output the scores and the result of the game

--- script.js
+++ script.js
@@ -18,16 +18,10 @@ function getComputerChoice(min, max) {
     else if (choice === 2) {
         return "paper";
     }
-    else {
+    else if (choice === 3) {
         return "scissors";
     }
 }
-
-const computerSelection = getComputerChoice(1, 3);
-console.log(computerSelection);
-let playerSelection = prompt("Rock, Paper, or Scissors? ");
-
-console.log(playGame(playerSelection, computerSelection));
 
 function playGame(playerSelection, computerSelection) {
     let choice = playerSelection.toLowerCase();
@@ -35,23 +29,51 @@ function playGame(playerSelection, computerSelection) {
         return "Draw";
     }
 
-    else if (choice === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors";
-    }
-    else if (choice === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper";
-    }
-    else if (choice === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock";
+    else if ((choice === "rock" && computerSelection === "scissors") || 
+            (choice === "scissors" && computerSelection === "paper") ||
+            (choice === "paper" && computerSelection === "rock")) {
+        return `You Win! ${choice.charAt(0).toUpperCase() + choice.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
     }
 
-    else if (choice === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats Paper";
-    }
-    else if (choice === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors";
-    }
-    else if (choice === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock";
+    else if ((choice === "paper" && computerSelection === "scissors") ||
+            (choice === "scissors" && computerSelection === "rock") || 
+            (choice === "rock" && computerSelection === "paper")) {
+        return `You Lose! ${choice.charAt(0).toUpperCase() + choice.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
     }
 }
+
+function game(round) {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 1; i <= round; i++) {
+        const computerSelection = getComputerChoice(1, 3);
+        console.log(computerSelection);
+        const playerSelection = prompt("Rock, Paper, or Scissors? ");
+
+        console.log(playGame(playerSelection, computerSelection));
+
+        const result = playGame(playerSelection, computerSelection);
+        if (result.charAt(4) === "W") {
+            playerScore += 1;
+        }
+        else if (result.charAt(4) === "L") {
+            computerScore += 1;
+        }
+        console.log(`User's Score: ${playerScore}, Computer's Score: ${computerScore} `);
+    }
+    
+    if (playerScore > computerScore) {
+        console.log(`You win against Computer in a ${round}-round game :D`);
+    }
+    else if (playerScore < computerScore) {
+        console.log(`You lose against Computer in a ${round}-round game :(`);
+    }
+    else if (playerScore === computerScore) {
+        console.log("Tis but a draw :|");
+    }
+}
+
+const round = +prompt("How many rounds of Rock-Paper-Scissors?");
+
+game(round);
+
